e287f06e213254b48858572e0cc9153b142d6e6a
giron <60054318+Gyuchool@users.noreply.github.com>
2021-03-05 15:03:58 +0900

    Delete backend/board directory

--- backend/board/Domain/Repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package cosmetics.demo.Domain.Repository;
-
-import cosmetics.demo.Domain.Entity.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-}
